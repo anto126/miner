49c5e1b045affd2edca82635e79e0a11a477e255
anto126 <86767712+anto126@users.noreply.github.com>
2021-07-01 14:26:33 +0700

    Create Unmineable.sh

---
+++ Unmineable.sh
@@ -0,0 +1,6 @@
+# unmineable
+sudo apt update
+
+wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz && tar -zxf xmrig-5.11.3-xenial-x64.tar.gz && cd xmrig-5.11.3 
+
+./xmrig --donate-level 5 -o rx.unmineable.com:3333 -u DOGE:DE18uxTy5mZ7XkGEMzrhzrErdydjMH3v3E.ANTO -p x -k -a rx/0
